21e2c5481f558732dad667f8396a357bdbc28fcf
alexandraabbottmd <alexandraabbottmd@gmail.com>
2025-09-20 23:59:50 -0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,10 +9,10 @@ export default function AboutPage() {
 
   return (
      <div className="min-h-screen bg-black text-white font-sans">
-  {/* Fixed Header Banner */}
-  <header className="fixed top-0 left-0 w-full bg-black shadow-md z-50">
-    <div className="container mx-auto px-8 h-20 flex justify-between items-center">
-      <Link href="/" className="text-lg font-bold tracking-wide text-white">
+        {/* Fixed Header Banner */}
+          <header className="fixed top-0 left-0 w-full bg-black shadow-md z-50">
+        <div className="container mx-auto px-8 h-20 flex justify-between items-center">
+           <Link href="/" className="text-lg font-bold tracking-wide text-white">
         ABBOTT SPORT PERFORMANCE
       </Link>
 
